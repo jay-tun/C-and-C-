0c5cc548a9d83d99614dc77227a0c12bf4c48dff
Jay Tun <146002359+jay-tun@users.noreply.github.com>
2024-04-02 12:05:39 +0200

    Create a9_p1.cpp

---
+++ Assignment9/a9_p1.cpp
@@ -0,0 +1,16 @@
+/*CH-230-A
+a9_p1.cpp
+Aung Tun
+atun@jacobs-university.de
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string s;
+    cout <<"Enter your country of origin: ";
+    cin >> s;
+    cout << "Your country of origin is: "<<s<<endl;
+    return 0;
+}
