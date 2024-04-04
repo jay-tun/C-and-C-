ec75b53b6f486dce8c8f3a6609e9b7ae2b5bdc22
Jay Tun <146002359+jay-tun@users.noreply.github.com>
2024-04-04 14:27:23 +0200

    Add files via upload

---
+++ Assignment8/a8_p3/queue.c
@@ -0,0 +1,93 @@
+/*
+CH-230-A
+a8 p3.c
+Aung Tun
+atun@jacobs-university.de
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+void initialize_queue(Queue *pq)
+{
+	pq->front = pq->rear = NULL;
+	pq->items = 0;
+}
+
+int queue_is_full(const Queue *pq)
+{
+	return pq->items == MAXQUEUE;
+}
+
+int queue_is_empty(const Queue *pq)
+{
+	return pq->items == 0;
+}
+
+int queue_item_count(const Queue *pq)
+{
+	return pq->items;
+}
+
+int enqueue(Item item, Queue *pq)
+{
+	Node *newelem;
+	newelem=(Node*)malloc(sizeof(Node));
+	if (newelem==NULL){
+		printf("Error allocating memory");
+		return -1;
+	}
+	newelem->item=item;
+	newelem->next=NULL;
+	if (queue_is_empty(pq)){
+		pq ->front = newelem;
+		pq ->rear  = pq->front;
+	}
+	else{
+		pq ->rear->next = newelem;
+		pq ->rear = pq->rear->next;
+	}
+	pq ->items=pq->items +1;
+    return 0;
+    
+}
+
+int dequeue(Item *pitem, Queue *pq)
+{
+	Node *tmp;
+	tmp=pq->front;
+	*pitem=pq->front->item;
+	pq->front=pq->front->next;
+	free(tmp);
+	
+	if (queue_is_empty(pq))
+	{
+		pq->front=NULL;
+		pq->rear=NULL;
+	}
+	pq->items=pq->items-1;
+	
+    return 0;
+}
+
+void empty_queue(Queue *pq)
+{
+	Item dummy;
+	while (!queue_is_empty(pq)) {
+		dequeue(&dummy, pq);
+	}
+}
+
+void printq(Queue pq){
+	printf("content of the queue: ");
+	Node *tmp;
+	tmp=pq.front;
+	for (int i = 0; i < pq.items; i++)
+	{
+		printf("%d ",tmp->item);
+		tmp=tmp->next;
+	}
+	
+	printf("\n");
+}
